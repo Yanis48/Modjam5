1c8015f3bba9dd223f2626e15a9de84c5818cb19
Yanis48 <doublecraft.official@gmail.com>
2021-04-21 18:28:44 +0200

    update to 1.16.5 & delete useless class

--- src/main/java/team/thegoldenhoe/cameraobscura/client/PhotoFilter.java
+++ src/main/java/team/thegoldenhoe/cameraobscura/client/PhotoFilter.java
@@ -3,5 +3,5 @@ package team.thegoldenhoe.cameraobscura.client;
 import java.awt.image.BufferedImage;
 
 public interface PhotoFilter {
-	public BufferedImage getFilteredImage(BufferedImage src);
+	BufferedImage getFilteredImage(BufferedImage src);
 }

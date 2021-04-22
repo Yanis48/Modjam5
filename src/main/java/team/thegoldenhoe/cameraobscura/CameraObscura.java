ae7933a726256ffa32d706d23ab0cd41f20800a2
Yanis48 <doublecraft.official@gmail.com>
2021-04-22 03:41:40 +0200

    add runtime mods for better testing & mod initializer

--- src/main/java/team/thegoldenhoe/cameraobscura/CameraObscura.java
+++ src/main/java/team/thegoldenhoe/cameraobscura/CameraObscura.java
@@ -1,32 +1,20 @@
 package team.thegoldenhoe.cameraobscura;
 
-import org.apache.logging.log4j.Logger;
+import net.fabricmc.api.ModInitializer;
+import team.thegoldenhoe.cameraobscura.init.*;
 
-import net.minecraft.block.Block;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
-import team.thegoldenhoe.cameraobscura.common.COGuiHandler;
-import team.thegoldenhoe.cameraobscura.common.CommonEvents;
-import team.thegoldenhoe.cameraobscura.common.CommonProxy;
-import team.thegoldenhoe.cameraobscura.common.capability.CameraCapabilities;
-import team.thegoldenhoe.cameraobscura.common.craftstudio.BlockFake;
-import team.thegoldenhoe.cameraobscura.common.craftstudio.BlockProps;
-import team.thegoldenhoe.cameraobscura.common.craftstudio.TileTypeMap;
-import team.thegoldenhoe.cameraobscura.common.network.CONetworkHandler;
-import team.thegoldenhoe.cameraobscura.utils.ModelHandler;
+public class CameraObscura implements ModInitializer {
 
-@Mod(modid = Info.MODID, name = Info.NAME, version = Info.VERSION)
-public class CameraObscura {
-    public static Logger logger;
+	@Override
+	public void onInitialize() {
+		new COBlocks();
+		new COBlockEntities();
+		new COItems();
+		new COItemGroups();
+		new COScreenHandlers();
+		new COSounds();
+	}
 
-    @Mod.Instance(Info.MODID)
-    public static CameraObscura instance;
 
     @SidedProxy(clientSide = Info.CLIENT_PROXY, serverSide = Info.SERVER_PROXY)
     public static CommonProxy proxy;
